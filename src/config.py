1e5fa09801fb2eacb3d9a770e1c6af1259d071c2
Delta-droid507 <tim.olff1994@gmail.com>
2025-11-25 06:53:50 +0100

    fix config.py to update RAW_DIR path for data organization

--- src/config.py
+++ src/config.py
@@ -1,12 +1,10 @@
-# src/config.py
 from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Project root directory (one level above src/)
 BASE_DIR = Path(__file__).resolve().parents[1]
 
-RAW_DIR = BASE_DIR / "raw"
+RAW_DIR = BASE_DIR / "data" / "raw"
 REF_DIR = BASE_DIR / "reference"
 
 ENV_PATH = BASE_DIR / "config" / ".env"
